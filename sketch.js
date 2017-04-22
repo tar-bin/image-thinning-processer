5b261599c33af3b08724979c0b091e77318ec647
tar_bin <tar.bin.master@gmail.com>
2017-04-22 23:55:25 +0900

    細線化のサンプル実装

---
+++ sketch.js
@@ -0,0 +1,120 @@
+var orgimg;
+
+function preload() {
+    orgimg = loadImage("test.png");
+}
+
+function setup() {
+    createCanvas(orgimg.width * 2, orgimg.height * 2);
+    image(orgimg, 0, 0);
+    // グレースケール
+    orgimg.filter(GRAY);
+    image(orgimg, orgimg.width, 0);
+    // 線画抽出
+    // http://www.mathgram.xyz/entry/cv/contour
+    // 白膨張 => 元と差を取る => 反転
+    let dilateimg = orgimg.get();
+    dilateimg.filter(DILATE);
+    dilateimg.filter(DILATE);
+    orgimg.blend(dilateimg,
+        0, 0, orgimg.width, orgimg.height,
+        0, 0, orgimg.width, orgimg.height,
+        DIFFERENCE);
+    // ２値化
+    orgimg.filter(THRESHOLD, 0.1);
+    image(orgimg, 0, orgimg.height);
+    // 細線化
+    NWG(orgimg);
+    orgimg.filter(INVERT);
+    image(orgimg, orgimg.width, orgimg.height);
+}
+
+function draw() {
+}
+
+// the Nagendraprasad-Wang-Gupta thinning algorithm
+// https://pdfs.semanticscholar.org/955d/f7f577fb337f12b72989a53f998ffc80ec6a.pdf
+function NWG(img) {
+    img.loadPixels();
+    const width = img.width;
+    const hight = img.height;
+    const pixels = img.pixels;
+
+    let updateFlag = true;
+    let g = 1;
+    // 100回、または更新がなくなるまで実行
+    while (updateFlag) {
+        // 更新フラグ(更新がない場合は終了)
+        updateFlag = false;
+        // 領域判定用は書き換わらないようにコピー
+        const tmpPixels = new Uint8Array(pixels);
+        // g
+        g = 1 - g;
+        // 全ピクセルループ
+        for (var y = 1; y < hight-1; y++) {
+            for (var x = 1; x < width-1; x++) {
+                // ピクセル位置を計算
+                var i = (y * width + x) * 4;
+                // 白領域のみを判定対象とする
+                if (tmpPixels[i]){
+                    // | p(7) | p(0) | p(1) |
+                    // | p(6) | P    | p(2) |
+                    // | p(5) | p(4) | p(3) |
+
+                    // 白 1 : 黒 0
+                    const p7 = (tmpPixels[i - width * 4 - 4]) ? 1 : 0;
+                    const p0 = (tmpPixels[i - width * 4    ]) ? 1 : 0;
+                    const p1 = (tmpPixels[i - width * 4 + 4]) ? 1 : 0;
+
+                    const p6 = (tmpPixels[i             - 4]) ? 1 : 0;
+                    const p2 = (tmpPixels[i             + 4]) ? 1 : 0;
+
+                    const p5 = (tmpPixels[i + width * 4 - 4]) ? 1 : 0;
+                    const p4 = (tmpPixels[i + width * 4    ]) ? 1 : 0;
+                    const p3 = (tmpPixels[i + width * 4 + 4]) ? 1 : 0;
+
+                    // 白の個数を算出
+                    let b = p0+p1+p2+p3+p4+p5+p6+p7;
+                    if (1 < b && b < 7){
+                        // a(p) の計算（隣接する白ピクセルの数)
+                        let a = 0;
+                        if (!p0 && p1) { a++; }
+                        if (!p1 && p2) { a++; }
+                        if (!p2 && p3) { a++; }
+                        if (!p3 && p4) { a++; }
+                        if (!p4 && p5) { a++; }
+                        if (!p5 && p6) { a++; }
+                        if (!p6 && p7) { a++; }
+                        if (!p7 && p0) { a++; }
+
+                        // c(p) の計算
+                        let c = 0;
+                        if (!p0 && !p1 && !p2 && !p5 && p4 && p6) {
+                            c = 1;
+                        }
+                        else if (!p2 && !p3 && !p4 && !p7 && p0 && p6) {
+                            c = 1;
+                        }
+                        
+                        if (a == 1 || c == 1) {
+                            // e(p)
+                            let e = (p2 + p4) * p0 * p6;
+                            // f(p)
+                            let f = (p0 + p6) * p4 * p2;
+                            // 黒に塗りつぶし
+                            if ((g == 0 && e == 0) || (g == 1 && f == 0)) {
+                                pixels[i    ] = 0; // R
+                                pixels[i + 1] = 0; // G
+                                pixels[i + 2] = 0; // B
+                                // 更新した場合は継続
+                                updateFlag = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    img.updatePixels();
+};
